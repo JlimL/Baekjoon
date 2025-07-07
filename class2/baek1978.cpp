ea305ee989be7860b5efa156a43736ff7184bd6b
limj <jeongliml2002@gmail.com>
2025-07-07 13:47:27 +0900

    class2 folder

---
+++ class2/baek1978.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	int N, result = 0, temp, cnt = 0;
+	cin >> N;
+	
+	for(int i=0; i<N; i++){
+		cin >> temp;
+		for(int div=1; div<=temp; div++){
+			if(temp%div==0) cnt++;
+		}
+		if(cnt==2){
+			result++;
+		}
+		cnt = 0;
+	}
+	
+	cout << result << '\n';
+	return 0;
+}
